025d99bc2eb6a9d4d300d1a407d8c08cb897dbe1
Igor Gorbanev <gorbanev_i@mail.ru>
2024-12-16 16:25:42 +0300

    header 5

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src="" alt=""/>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Services</a></li>
+                </ul>
+                <button>Contact Me</button>
+            </nav>
+        </header>
+    );
+};
