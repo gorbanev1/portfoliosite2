594f38b735d2128c632e78a3b28adc2fffbe5e94
Igor Gorbanev <gorbanev_i@mail.ru>
2025-01-13 17:04:11 +0300

    refactoring 6

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -1,4 +1,4 @@
-import React, {Children, cloneElement, useEffect, useState} from 'react';
+import React, {Children, cloneElement, ReactChildren, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import photoReview from '../../../assets/images/reviewman.webp'
@@ -24,6 +24,26 @@ const arr = [
         value: "743"
     }
 ]
+const reviews = [
+    {
+        iconId: photoReview,
+        reviewText: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum\n" +
+            "                            est\n" +
+            "                            a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus,\n" +
+            "                            ut\n" +
+            "                            interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.\n" +
+            "                            Class\n" +
+            "                            aptent taciti sociosqu ad litora torquent",
+    },
+    {
+        iconId: photoReview2,
+        reviewText: " sadfsdafsdafsdaf  sdaf safsdaf asf sadf sadfsd fsdf sf s safdf sdaaaa                sdafafdsa sdaf asf sadf dsaf  sdfsdaf assdaf sdaf fdsaf sfsdaf sa",
+    },
+    {
+        iconId: photoReview,
+        reviewText: " ываыва ыва ываыв авыа ыв аыва авыаыва ываывываыва ыва ываыв авыа ыв аыва авыаыва ываывываыва ыва ываыв авыа ыв аыва авыаыва ываывываыва ыва ываыв авыа ыв аыва авыаыва ываывываыва ыва ываыв авыа ыв аыва авыаыва ываывываыва ыва ываыв авыа ыв аыва авыаыва ываывываыва ыва ываыв авыа ыв аыва авыаыва ываыв",
+    }
+]
 
 
 export const Testimony = () => {
@@ -32,8 +52,17 @@ export const Testimony = () => {
         <StyledTestimony>
             <Container>
                 <FlexWrapper padding={"100px 0"} wrap={"wrap"}>
+
                     <Carousel>
-                        <img src={photoReview}/>
+                        {reviews.map((review, index) => {
+                            return <Photo width={"390px"} height={"390px"} src={review.iconId}/>
+                        })}
+                        {reviews.map((review, index) => {
+                            return <Text fontSize={"20px"} fontWeight={"700"} lineHeight={"1.8"}
+                                         margin={"35px 0 0 "}>{review.reviewText}</Text>
+                        })}
+
+                        {/*                       <img src={photoReview}/>
                         <img src={photoReview2}/>
                         <img src={photoReview}/>
                         <div> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum
@@ -53,9 +82,9 @@ export const Testimony = () => {
                             aptent taciti sociosqu ad litora torquent</div>
 
 
-                        {/*<Photo width={"390px"} height={"390px"} src={photoReview}/>*/}
-                        {/*<Photo width={"390px"} height={"390px"} src={photoReview}/>*/}
-                        {/*<Photo width={"390px"} height={"390px"} src={photoReview}/>*/}
+                        <Photo width={"390px"} height={"390px"} src={photoReview}/>
+                        <Photo width={"390px"} height={"390px"} src={photoReview}/>
+                        <Photo width={"390px"} height={"390px"} src={photoReview}/>*/}
                     </Carousel>
                     {/*<Photo width={"390px"} height={"390px"} src={photoReview}/>*/}
                     <FlexWrapper direction={"column"} margin={"0 108px   0 12px"} width={"490px"}>
@@ -132,75 +161,122 @@ export const Carousel = ({children}) => {
         console.log("dsfsdfsdfdRRRRRRRRRR")
         setOffset((currentOffset) => {
             const newOffset = currentOffset - PAGE1_WIDTH
+            const maxOffset = -(PAGE1_WIDTH * (pages.length - 1))
             console.log(offset)
-            return newOffset
+            return Math.max(newOffset, maxOffset)
         })
         setOffset2((currentOffset) => {
             const newOffset = currentOffset - PAGE2_WIDTH
+            const maxOffset = -(PAGE2_WIDTH * (pages2.length - 1))
             console.log(offset)
-            return newOffset
+            return Math.max(newOffset, maxOffset)
         })
 
     }
 
-    const FilterEls: any = (elType: any) => {
-        return {}
+    const filterElements = (pageWidth: number, elementType: string) => {
+        return Children.map(children, (child) => {
+            return cloneElement((child), {
+                style: {
+                    height: "100%",
+                    minWidth: `${pageWidth}px`,
+                    maxWidth: `${pageWidth}px`,
+                }
+            })
+        }).filter((el: any) => el.type.target === elementType)
+        //render.target
     }
     useEffect(() => {
         setPages(
-            Children.map(children, (child) => {
-                return cloneElement((child), {
-                    style: {
-                        height: "100%",
-                        minWidth: `${PAGE1_WIDTH}px`,
-                        maxWidth: `${PAGE1_WIDTH}px`,
-                    }
-                })
-            }).filter((el: any) => el.type === "img")
+            filterElements(PAGE1_WIDTH, "img")
+            /* Children.map(children, (child) => {
+                 return cloneElement((child), {
+                     style: {
+                         height: "100%",
+                         minWidth: `${PAGE1_WIDTH}px`,
+                         maxWidth: `${PAGE1_WIDTH}px`,
+                     }
+                 })
+             }).filter((el: any) => el.type === "img")*/
         )
     }, [])
     useEffect(() => {
-        setPages2(
-            Children.map(children, (child) => {
-                return cloneElement((child), {
-                    style: {
-                        height: "100%",
-                        minWidth: `${PAGE2_WIDTH}px`,
-                        maxWidth: `${PAGE2_WIDTH}px`,
-                    }
-                })
-            }).filter((el: any) => el.type === "div")
+        setPages2(filterElements(PAGE2_WIDTH, "p")
+            /*   Children.map(children, (child) => {
+                   return cloneElement((child), {
+                       style: {
+                           height: "100%",
+                           minWidth: `${PAGE2_WIDTH}px`,
+                           maxWidth: `${PAGE2_WIDTH}px`,
+                       }
+                   })
+               }).filter((el: any) => el.type === "div")*/
         )
     }, [])
+    console.log(pages)
     console.log(pages2)
-    return (
-        <>
-            <MainContainer>
+    console.log(Children + "dadasds")
+    const sliderPages = [
+        {
+            pages: pages,
+            offset,
+            width: "390px"
+        },
+        {
+            pages: pages2,
+            offset,
+            width: "490px"
+        }
+    ]
+    return (/*<>
+            {sliderPages.map((page, index) => {
 
-                <WatchWindow>
-                    <AllPagesContainer style={{
-                        transform: `translateX(${offset}px)`
-                    }}>{pages}</AllPagesContainer>
-                </WatchWindow>
-            </MainContainer>
+                return <>
+                    <MainContainer width={page.width}>
+                        <WatchWindow>
+                            <AllPagesContainer style={{
+                                transform: `translateX(${page.offset}px)`
+                            }}>{page.pages}</AllPagesContainer>
+                        </WatchWindow>
+                    </MainContainer>
+                    {(index===1)&&<div>fsdDDDDDDDDDfsd</div>}
+                </>
 
-            <MainContainer2>
-                <WatchWindow>
-                    <AllDivsContainer style={{
-                        transform: `translateX(${offset2}px)`
-                    }}>{pages2}</AllDivsContainer>
-                </WatchWindow>
-            </MainContainer2>
-            <button onClick={handleLeftArrowClick}>
-                <Icon iconId={"leftArrow"}/></button>
-            <button onClick={handleRightArrowClick}>
-                <Icon iconId={"rightArrow"}/>
-            </button>
-        </>
+            })}
+        </>*/
+         <>
+             <MainContainer width={"390px"}>
+                 <WatchWindow>
+                     <AllPagesContainer style={{
+                         transform: `translateX(${offset}px)`
+                     }}>{pages}</AllPagesContainer>
+                 </WatchWindow>
+             </MainContainer>
+             <FlexWrapper direction={"column"}>
+             <MainContainer width={"490px"}>
+                 <WatchWindow>
+                     <AllDivsContainer style={{
+                         transform: `translateX(${offset2}px)`
+                     }}>{pages2}</AllDivsContainer>
+                 </WatchWindow>
+             </MainContainer >
+                 <FlexWrapper>
+             <button onClick={handleLeftArrowClick}>
+                 <Icon iconId={"leftArrow"}/></button>
+             <button onClick={handleRightArrowClick}>
+                 <Icon iconId={"rightArrow"}/>
+             </button>
+                 </FlexWrapper>
+             </FlexWrapper>
+         </>
     );
 };
+type MainContainerPropsType = {
+    width: string
+    height?: string
+}
 
-const MainContainer = styled.div`
+const MainContainer = styled.div<MainContainerPropsType>`
   width: 390px;
   height: 390px;
 `
@@ -213,11 +289,7 @@ const WatchWindow = styled.div`
   width: 100%;
   overflow: hidden;
 `
-const WatchWindow2 = styled.div`
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-`
+
 const AllPagesContainer = styled.div`
   height: 100%;
   display: flex;
